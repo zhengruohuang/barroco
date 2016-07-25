8eef91cc25aaf5217cda2a0bb80f2a238e338acf
Ruohuang Zheng <zhengruohuang@gmail.com>
2016-07-24 22:22:26 -0700

    Initial commit

---
+++ rtl/verilog/config.v
@@ -0,0 +1,13 @@
+`define CORE_COUNT      4
+
+`define SMT_WIDTH       2
+`define FETCH_WIDTH     4
+`define DECODE_WIDTH    4
+`define RENAME_WIDTH    4
+`define DISPATCH_WIDTH  4
+`define ISSUE_WIDTH     8
+`define RETIRE_WIDTH    8
+`define COMMIT_WIDTH    8
+
+`define FETCH_BUFFER    16
+`define PREG_COUNT      512
